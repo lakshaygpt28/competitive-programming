97c4228a17dffb4c89d1f5d466db97819dcfefdd
Lakshay Gupta <lakshaygpt28@gmail.com>
2017-12-29 10:49:18 +0530

    Codeforces Round #455 (Div. 2)

---
+++ codeforce/generate login.py
@@ -0,0 +1,10 @@
+string = input().split()
+print (string[0][0], end = '')
+for x in range(1,len(string[0])):
+	#print (x)
+	if string[0][x] < string[1][0]:
+		print (string[0][x], end = '')
+	else:
+		break	
+print (string[1][0])
+#print (string[0][2] < string[1][0])
